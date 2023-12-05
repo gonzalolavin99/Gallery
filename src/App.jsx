7ccb94c9fe503d2a061e743796e41db13cfd3d2e
gonzalolavin99 <gonzalolavin99@gmail.com>
2023-12-04 23:11:39 -0300

    Paquetes y componentes creados

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
 function App() {
-
   return (
     <>
-   
+      <MyContextProvider>
+
+      </MyContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
